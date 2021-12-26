1eb128981388bc6255b492242ad0cec447edce94
SANZONGi <2577786020@qq.com>
2021-12-26 10:54:35 +0800

    gateway

---
+++ gateway-service/src/main/java/com/example/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author SANZONG
+ * @date 2021/12/26
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
