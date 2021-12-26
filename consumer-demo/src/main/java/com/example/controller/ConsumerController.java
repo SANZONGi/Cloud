d523cade3db71a6da8399cc49f8a29c062b17e7f
SANZONGi <2577786020@qq.com>
2021-12-26 09:36:58 +0800

    hystrix

--- consumer-demo/src/main/java/com/example/controller/ConsumerController.java
+++ consumer-demo/src/main/java/com/example/controller/ConsumerController.java
@@ -1,8 +1,8 @@
 package com.example.controller;
 
-import com.example.entity.User;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,25 +14,28 @@ import org.springframework.web.client.RestTemplate;
  */
 @RestController
 @RequestMapping("/consumer")
+@DefaultProperties(defaultFallback = "fallAll")
 public class ConsumerController {
     private RestTemplate restTemplate;
-    private DiscoveryClient discoveryClient;
 
     @Autowired
     public void setRestTemplate(RestTemplate restTemplate){
         this.restTemplate=restTemplate;
     }
 
-    @Autowired
-    public void setDiscoveryClient(DiscoveryClient discoveryClient) {
-        this.discoveryClient = discoveryClient;
+    @GetMapping("/{id}")
+    @HystrixCommand
+    public String getUser(@PathVariable int id) {
+        String url = "http://user-service/user/"+id;
+        return restTemplate.getForObject(url,String.class);
     }
 
+    public String findUserFallBack(int id) {
+        return "Unavailable";
+    }
 
-    @GetMapping("/{id}")
-    public User getUser(@PathVariable int id) {
-        String url = "http://user-service/user/"+id;
-        return restTemplate.getForObject(url,User.class);
+    public String fallAll() {
+        return "all unavailable";
     }
 }
 
--- user-service/src/main/java/com/example/controller/UserController.java
+++ user-service/src/main/java/com/example/controller/UserController.java
@@ -3,8 +3,10 @@ package com.example.controller;
 import com.example.entity.User;
 import com.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author SANZONG
@@ -15,17 +17,16 @@ public class UserController {
 
     private UserService userService;
 
-    @Value("${server.port}")
-    int port;
-
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable int id){
-        System.out.println(port);
+    public User getUserById(@PathVariable int id) {
+        if (id == 1){
+            throw new RuntimeException();
+        }
         return userService.findUserById(id);
     }
 }
